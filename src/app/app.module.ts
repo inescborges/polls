dc186f3d400cd7df43419947d421aeeee3e5bda2
Ines Borges <ines.borges@blueskysolutionslda.onmicrosoft.com>
2020-02-26 13:50:26 +0000

    loading screen feature

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,28 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { CommonModule } from '@angular/common';  
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http'; 
+import { HTTP_INTERCEPTORS } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
+import { LoaderComponent } from './loader/loader.component';
+import { LoaderService } from './services/loader.service';
+import { LoaderInterceptor } from './interceptors/loader.interceptor';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoaderComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule,
+    CommonModule
   ],
-  providers: [],
+  providers: [
+    LoaderService,
+    { provide: HTTP_INTERCEPTORS, useClass: LoaderInterceptor, multi: true }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/loader/loader.component.ts
@@ -0,0 +1,38 @@
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Subscription} from 'rxjs';
+import { HttpClient } from '@angular/common/http';
+import { LoaderService } from '../services/loader.service';
+@Component({
+  selector: 'app-loader',
+  templateUrl: './loader.component.html',
+  styleUrls: ['./loader.component.css']
+})
+export class LoaderComponent implements OnInit, OnDestroy {
+  loadingOK = false;
+  showContent = false;
+  loadingSubscription: Subscription;
+  constructor(private http: HttpClient, private loaderService: LoaderService){ }
+  ngOnInit() {
+    this.loadingSubscription = this.loaderService.isLoading.subscribe((value) => {
+      this.loadingOK = value;
+    });
+    this.callApi();
+  }
+  
+  callApi() {
+    this.http.get('https://private-anon-f4b8190d8a-blissrecruitmentapi.apiary-mock.com/health')
+    .subscribe(data => {
+      console.log(data);
+        if(data.hasOwnProperty('status')) {
+          if(data['status'] === 'OK') {
+            this.showContent = true;
+          }
+          console.log(this.loadingOK)
+        }
+    });
+  }
+
+  ngOnDestroy() {
+    this.loadingSubscription.unsubscribe();
+  }
+}
